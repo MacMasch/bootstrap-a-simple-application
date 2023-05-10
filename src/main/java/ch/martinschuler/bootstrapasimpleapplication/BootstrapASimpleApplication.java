0addaa28ee0ced7681f514d1cf0b8a7b38fb3d3c
Martin Schuler <martinschuler@mac.com>
2023-05-10 06:36:29 +0200

    Initial Commit

---
+++ src/main/java/ch/martinschuler/bootstrapasimpleapplication/BootstrapASimpleApplication.java
@@ -0,0 +1,13 @@
+package ch.martinschuler.bootstrapasimpleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapASimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapASimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/martinschuler/bootstrapasimpleapplication/BootstrapASimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.martinschuler.bootstrapasimpleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapASimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
