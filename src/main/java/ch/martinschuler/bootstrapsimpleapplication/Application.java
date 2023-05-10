7499ef12faa6491cfcc6086a84f2e8587e0bb9cd
Martin Schuler <martinschuler@mac.com>
2023-05-10 06:47:14 +0200

    Application initialized

--- src/main/java/ch/martinschuler/bootstrapsimpleapplication/Application.java
+++ src/main/java/ch/martinschuler/bootstrapsimpleapplication/Application.java
@@ -1,13 +1,13 @@
-package ch.martinschuler.bootstrapasimpleapplication;
+package ch.martinschuler.bootstrapsimpleapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootstrapASimpleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootstrapASimpleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
