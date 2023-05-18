23da162fa395251493b3d1e2fef20c71bf26c40b
Martin Schuler <martinschuler@mac.com>
2023-05-18 14:15:47 +0200

    chore: Use buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.0.6"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("spring-conventions")
 }
 
 version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
 
 buildscript {
     repositories {
@@ -16,24 +19,13 @@ buildscript {
 }
 apply(plugin = "com.dipien.semantic-version")
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
